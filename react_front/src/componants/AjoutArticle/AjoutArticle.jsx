aad030b18c3fae9abf502dae2f9127e1873da2f1
Abdel1117 <abderahmane.adjali@live.fr>
2023-05-16 23:14:20 +0200

    push

--- react_front/src/componants/AjoutArticle/AjoutArticle.jsx
+++ react_front/src/componants/AjoutArticle/AjoutArticle.jsx
@@ -1,35 +1,166 @@
-import React ,{ useState} from 'react'
+import React, { useState } from 'react'
 
 export default function AjoutArticle() {
-    const [article, setArticle] = useState({})
-    const [Titre, setTitre] = useState("")
-    const [Article, setArticlePara] = useState("")
-    const [Slug, setSlug] = useState("")
-    
+  const [article, setArticle] = useState({});
+  const [Titre, setTitre] = useState("");
+  const [Slug, setSlug] = useState("");
+  const [sections, setSections] = useState([
+
+  ]);
+
+  const ajouterSection = () => {
+    const nouvelleSection = {
+      titre: "",
+      contenu: ""
+    };
+    setSections([...sections, nouvelleSection])
+  }
+
+  const handleSlugChange = (e) => {
+    const slug = e.target.name;
+    const checked = e.target.checked;
+
+    if (checked) {
+      setSlug((prevSlug) => [...prevSlug, slug]);
+    } else {
+      setSlug((prevSlug) => prevSlug.filter((prevSlug) => prevSlug !== slug))
+    }
+  }
+
+  const handleSubmit = () => {
+    const nouvelArticle = {
+      titre: Titre,
+      contenu: sections,
+      slugs: Slug,
+
+    }
+
+    console.log(nouvelArticle)
+  }
+
+
   return (
-    <section className={`border w-100 h-full bg-slate-500 transition-all `}>
-      
-<form>
-  <div class="mb-6">
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-    <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-  </div>
-  <div class="mb-6">
-    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-    <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-  </div>
-  <div class="mb-6">
-    <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
-    <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-  </div>
-  <div class="flex items-start mb-6">
-    <div class="flex items-center h-5">
-      <input id="terms" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-    </div>
-    <label for="terms" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
-  </div>
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
-</form>
+    <section className={` `}>
+
+      <form onSubmit={() => { handleSubmit() }} className="w-[280px] sm:w-[520px] md:w-[500px] lg:w-10/12 h-full bg-white dark:bg-blue-900 rounded-lg p-4 mx-auto">
+        <div class="mb-6">
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre</label>
+          <input onChange={(e) => { setTitre(e.target.value) }} type="text" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Titre de l'article" required />
+        </div>
+        <div class="mb-6">
+          <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Slug</label>
+          <fieldset className=' grid grid-cols-2 md:grid-cols-3'>
+            <legend className="text-sm md:text-md dark:text-white">Choisisez au moins une catégorie </legend>
+
+            <div>
+              <input
+                className=''
+                type="checkbox"
+                id="Planète"
+                name="Planète"
+                onChange={handleSlugChange}
+                checked={Slug.includes('Planète')}
+              />
+              <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Planète">Planète</label>
+            </div>
+
+            <div>
+              <input
+                className=''
+                type="checkbox"
+                id="Systeme_Solaire"
+                name="Systeme_Solaire"
+                value="Systeme Solaire"
+                onChange={handleSlugChange}
+                checked={Slug.includes('Systeme_Solaire')}
+              />
+              <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Systeme_Solaire">Système Solaire</label>
+            </div>
+
+            <div>
+              <input
+                className=''
+                type="checkbox"
+                id="Objet_Stélaire"
+                name="Objet_Stélaire"
+                value="Objet Stélaire"
+                onChange={handleSlugChange}
+                checked={Slug.includes('Objet_Stélaire')}
+              />
+              <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Objet_Stélaire">Objet Stélaire</label>
+            </div>
+
+            <div>
+              <input
+                className=''
+                type="checkbox"
+                id="Etoile"
+                name="Etoile"
+                value="Etoile"
+                onChange={handleSlugChange}
+                checked={Slug.includes('Etoile')}
+              />
+              <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Etoile">Etoile</label>
+            </div>
+
+            <div>
+              <input
+                className=''
+                type="checkbox"
+                id="Météorite"
+                name="Météorite"
+                value="Météorite"
+                onChange={handleSlugChange}
+                checked={Slug.includes('Météorite')}
+              />
+              <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Météorite">Météorite</label>
+            </div>
+          </fieldset>
+
+        </div>
+        {sections.map((section, index) => (
+          <div key={index}>
+            <div class="mb-6">
+              <label htmlFor={`Section_titre_${index}`} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre de la Section</label>
+              <input
+                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
+                name={`Section_titre_${index}`}
+                id={`Section_titre_${index}`}
+                value={section.titre}
+                onChange={(e) => {
+                  const updatedSections = [...sections];
+                  updatedSections[index].titre = e.target.value;
+                  setSections(updatedSections);
+                }}
+              />
+            </div>
+            <div class="mb-6">
+              <label htmlFor={`Section_${index}`} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Section</label>
+              <textarea
+                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
+                name={`Section_${index}`}
+                id={`Section_${index}`}
+                value={section.contenu}
+                onChange={(e) => {
+                  const updatedSections = [...sections];
+                  updatedSections[index].contenu = e.target.value;
+                  setSections(updatedSections);
+                }}
+                cols="30"
+                rows="10"
+              ></textarea>
+            </div>
+          </div>
+        ))}
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-3' >
+          <a href="#" onClick={() => { ajouterSection() }} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >
+            Ajouter une Section
+          </a>
+          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Publier article</button>
+
+
+        </div>
+      </form>
 
     </section>
   )
--- react_front/src/pages/Ajouter_Entity/Ajouter_Entity.jsx
+++ react_front/src/pages/Ajouter_Entity/Ajouter_Entity.jsx
@@ -1,24 +1,24 @@
-import React, {useState } from 'react'
+import React, { useState } from 'react'
 import AjoutArticle from '../../componants/AjoutArticle/AjoutArticle';
 
 export default function Ajouter_Entity() {
-    const [Action, SetAction] = useState("");
-    let ComponantToRender 
-    switch (Action) {
-        case "Article":
-          ComponantToRender =  <AjoutArticle />
-            break;
-        case "Image" :
-          ComponantToRender =  <AjouterImage />
-        case "Video" :
-          ComponantToRender =  <AjouterVideo />
-        default:
-            break;
-    }
+  const [Action, SetAction] = useState("");
+  let ComponantToRender
+  switch (Action) {
+    case "Article":
+      ComponantToRender = <AjoutArticle />
+      break;
+    case "Image":
+      ComponantToRender = <AjouterImage />
+    case "Video":
+      ComponantToRender = <AjouterVideo />
+    default:
+      break;
+  }
 
   return (
-    <section>
-        <AjoutArticle />
+    <section className='py-10'>
+      <AjoutArticle />
 
 
     </section>
