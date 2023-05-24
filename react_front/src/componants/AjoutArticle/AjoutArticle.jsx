b0bd041d1e14e28f9c4ef56fc376a83ee79d734b
Abdel1117 <abderahmane.adjali@live.fr>
2023-05-24 18:04:14 +0200

    push

--- react_front/src/componants/AjoutArticle/AjoutArticle.jsx
+++ react_front/src/componants/AjoutArticle/AjoutArticle.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message';
+import _ from "lodash/fp";
 
 export default function AjoutArticle() {
   const [article, setArticle] = useState({});
   const [Titre, setTitre] = useState("");
-  const [Slug, setSlug] = useState("");
+  const [Slug, setSlug] = useState([]);
   const [sections, setSections] = useState([
 
   ]);
 
-  const ajouterSection = () => {
+  const { register, formState: { errors }, handleSubmit, getValues, watch, setValue } = useForm({
+    criteriaMode: 'all',
+
+  });
+
+  const addSection = () => {
     const nouvelleSection = {
       titre: "",
       contenu: ""
     };
     setSections([...sections, nouvelleSection])
   }
+  const deleteLastSection = () => {
+
+    const newState = [...sections]
+    console.log(newState)
+    newState.pop()
+    console.log(newState)
+    setSections(setSections(newState))
+  }
+
+
 
   const handleSlugChange = (e) => {
     const slug = e.target.name;
@@ -22,31 +40,56 @@ export default function AjoutArticle() {
 
     if (checked) {
       setSlug((prevSlug) => [...prevSlug, slug]);
+
     } else {
       setSlug((prevSlug) => prevSlug.filter((prevSlug) => prevSlug !== slug))
     }
+
+    setValue('Slug', (prevSlug) => {
+      if (checked) {
+        return [...prevSlug, slug];
+      } else {
+        return prevSlug.filter((prevSlug) => prevSlug !== slug);
+      }
+    });
   }
 
-  const handleSubmit = () => {
+  const handleForm = () => {
     const nouvelArticle = {
       titre: Titre,
       contenu: sections,
       slugs: Slug,
 
     }
-
+    console.log("ICI")
+    setArticle((prevArticle) => ({ ...prevArticle, nouvelArticle }))
   }
 
 
   return (
     <section className={` `}>
 
-      <form onSubmit={() => { handleSubmit() }} className="w-[280px] sm:w-[520px] md:w-[500px] lg:w-10/12 h-full bg-white dark:bg-blue-900 rounded-lg p-4 mx-auto">
-        <div class="mb-6">
-          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre</label>
-          <input onChange={(e) => { setTitre(e.target.value) }} type="text" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Titre de l'article" required />
+      <form onSubmit={handleSubmit(handleForm)} className="w-[280px] sm:w-[520px] md:w-[500px] lg:w-10/12 h-full bg-white dark:bg-blue-900 rounded-lg p-4 mx-auto">
+        <div className="mb-6">
+          <label htmlFor="Titre_Article" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre</label>
+          <input
+            {...register('Article_title', {
+              required: "Veuillez remplir ce champs avec un Titre",
+              pattern: {
+                value: /^(?! )[a-zA-Z0-9\-() ]{1,18}(?<! )$/,
+                message: "Veuillez taper un titre qui contient 3 à 20 caractères "
+
+              }
+            })}
+            onChange={(e) => { setTitre(e.target.value) }}
+            type="text"
+            name="Article_title"
+            id="Titre_Article"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            placeholder="Titre de l'article"
+            required />
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Slug</label>
           <fieldset className=' grid grid-cols-2 md:grid-cols-3'>
             <legend className="text-sm md:text-md dark:text-white">Choisisez au moins une catégorie </legend>
@@ -56,7 +99,9 @@ export default function AjoutArticle() {
                 className=''
                 type="checkbox"
                 id="Planète"
+                {...register('Slug', { required: true })}
                 name="Planète"
+                value="Planète"
                 onChange={handleSlugChange}
                 checked={Slug.includes('Planète')}
               />
@@ -68,10 +113,12 @@ export default function AjoutArticle() {
                 className=''
                 type="checkbox"
                 id="Systeme_Solaire"
-                name="Systeme_Solaire"
+                {...register('Slug', { required: true })}
+                checked={Slug.includes('Systeme Solaire')}
+                name="Systeme Solaire"
                 value="Systeme Solaire"
                 onChange={handleSlugChange}
-                checked={Slug.includes('Systeme_Solaire')}
+
               />
               <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Systeme_Solaire">Système Solaire</label>
             </div>
@@ -81,10 +128,12 @@ export default function AjoutArticle() {
                 className=''
                 type="checkbox"
                 id="Objet_Stélaire"
-                name="Objet_Stélaire"
+                {...register('Slug', { required: true })}
+                name="Objet Stélaire"
                 value="Objet Stélaire"
                 onChange={handleSlugChange}
                 checked={Slug.includes('Objet_Stélaire')}
+
               />
               <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Objet_Stélaire">Objet Stélaire</label>
             </div>
@@ -94,10 +143,12 @@ export default function AjoutArticle() {
                 className=''
                 type="checkbox"
                 id="Etoile"
+                {...register('Slug', { required: true })}
                 name="Etoile"
                 value="Etoile"
                 onChange={handleSlugChange}
                 checked={Slug.includes('Etoile')}
+
               />
               <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Etoile">Etoile</label>
             </div>
@@ -107,60 +158,100 @@ export default function AjoutArticle() {
                 className=''
                 type="checkbox"
                 id="Météorite"
+                {...register('Slug', { required: true })}
                 name="Météorite"
                 value="Météorite"
-                onChange={handleSlugChange}
                 checked={Slug.includes('Météorite')}
+                onChange={handleSlugChange}
+
               />
               <label className='text-sm md:text-md dark:text-white ml-1' htmlFor="Météorite">Météorite</label>
+            </div>
+            <div className=' '>
+
+              {errors.Slug && (
+                <p className="text-red-600">Veuillez sélectionner au moins un Slug pour l'article en question</p>
+              )}
+
+
             </div>
           </fieldset>
 
         </div>
-        {sections.map((section, index) => (
+        {sections?.map((section, index) => (
           <div key={index}>
-            <div class="mb-6">
+            <div className="mb-6">
               <label htmlFor={`Section_titre_${index}`} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre de la Section</label>
               <input
                 className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
                 name={`Section_titre_${index}`}
                 id={`Section_titre_${index}`}
                 value={section.titre}
+
+                {...register(`Section_titre${index}`, {
+                  required: "Veuillez taper un titre de section",
+                  pattern: {
+                    required: true,
+                    value: /^(?! )[a-zA-Z0-9\-() ]{1,38}(?<! )$/,
+                    message: "Veuillez taper un Titrre qui contien au moins 3 caractères et au maxmimum 20 caractères"
+                  }
+                })}
                 onChange={(e) => {
                   const updatedSections = [...sections];
                   updatedSections[index].titre = e.target.value;
                   setSections(updatedSections);
                 }}
               />
+              <>
+                {errors[`Section_titre${index}`] && (
+                  <p>{errors[`Section_titre${index}`]?.message}</p>
+                )}
+              </>
             </div>
-            <div class="mb-6">
+            <div className="mb-6">
               <label htmlFor={`Section_${index}`} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Section</label>
               <textarea
                 className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
+                cols="30"
+                rows="10"
                 name={`Section_${index}`}
                 id={`Section_${index}`}
                 value={section.contenu}
+
+                {...register(`Section_${index}`, {
+                  required: "Veuillez taper une section d'article",
+                  pattern: {
+                    required: true,
+                    value: /^.{200,800}$/,
+                    message: "Veuillez taper une section d'article qui contiens au moins 200 caractères et au maxmimum 800 caractères"
+                  }
+                })}
                 onChange={(e) => {
                   const updatedSections = [...sections];
                   updatedSections[index].contenu = e.target.value;
                   setSections(updatedSections);
                 }}
-                cols="30"
-                rows="10"
               ></textarea>
+              <>
+                {errors[`Section_${index}`] && (
+                  <p>{errors[`Section_${index}`]?.message}</p>
+                )}
+              </>
             </div>
           </div>
         ))}
         <div className='grid grid-cols-1 md:grid-cols-2 gap-3' >
-          <a href="#" onClick={() => { ajouterSection() }} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >
+          <a href="#" onClick={() => { addSection() }} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >
             Ajouter une Section
           </a>
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Publier article</button>
+
+          <a href="#" onClick={() => { deleteLastSection() }} className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Supprimer la derniere section</a>
+          <button onClick={() => { handleSubmit() }} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Publier article</button>
 
 
         </div>
       </form>
 
-    </section>
+    </section >
   )
 }
