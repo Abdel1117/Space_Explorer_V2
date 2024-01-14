f3a67dd45fdafd6f4cce664ebf66bc04007ecf06
Abdel1117 <abderahmane.adjali@live.fr>
2024-01-14 23:20:58 +0100

    push for circleci

--- react_front/__tests__/GalerieTest.test.jsx
+++ react_front/__tests__/GalerieTest.test.jsx
@@ -4,5 +4,8 @@ import { render, screen } from '@testing-library/react';
 describe("Galerie", () => {
     it("render Galerie page with image", () => {
         render(<Galerie />)
+        const galerieElement = screen.getAllByText("Moins d'image")
+        expect(galerieElement.length).toBeGreaterThan(0);
+
     })
 });
--- react_front/__tests__/HomeTest.test.jsx
+++ react_front/__tests__/HomeTest.test.jsx
@@ -1,8 +1,14 @@
+import { expect } from "vitest";
 import Home from "../src/pages/Home/Home"
 import { render, screen } from '@testing-library/react';
 
 describe("Home", () => {
     it("render home page test", () => {
         render(<Home />)
+        const homeElement = screen.getAllByText("Mon Titre")
+        expect(homeElement.length).toBeGreaterThan(0);
+        expect(homeElement[0]).toHaveTextContent("Mon Titre");
+
+
     })
 });
