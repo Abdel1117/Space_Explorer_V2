cce9101939c64821b67a42d2968b4c78a1c6d9fa
Abdel1117 <abderahmane.adjali@live.fr>
2024-01-14 23:53:20 +0100

    push to fix build of frontend

--- react_front/src/pages/AjouterImage/Ajouter_Image.jsx
+++ react_front/src/pages/AjouterImage/Ajouter_Image.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import AjoutImage from "../../componants/AjoutImage/AjoutImage"
+import AjoutImage from '../../componants/AjoutImage/AjoutImage'
+
+
 export default function Ajouter_Image() {
     return (
         <section className="flex flex-col items-center min-h-screen pt-8">
