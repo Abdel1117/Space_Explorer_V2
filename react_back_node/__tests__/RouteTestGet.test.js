1ffe286bb38dbaa728928bd860b50794d6573bd0
Abdel1117 <abderahmane.adjali@live.fr>
2024-02-25 12:29:18 +0100

    commit of new circleCi

--- react_back_node/__tests__/RouteTestGet.test.js
+++ react_back_node/__tests__/RouteTestGet.test.js
@@ -1,4 +1,4 @@
-const request = require("supertest");
+/* const request = require("supertest");
 const app = require("../index");
 require("dotenv").config()
 const mongoose = require("mongoose")
@@ -30,3 +30,4 @@ afterAll(async () => {
     await mongoose.disconnect();
 
 });
+ */
